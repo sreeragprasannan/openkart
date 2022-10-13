7b4aecf9542afbc225c3b0c9efff882998d83384
sreerag <sreeraaag@gmail.com>
2022-10-13 15:27:05 +0530

    9th commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from myapp.models import Product
+from django.db.models import Q
 # Create your views here.
 
-def index(request):
-    
-    li = ["Alllen","Sreerag","alwin","justin","allu"]
-    
-    context = {'names':li}
-    
-    return render(request, 'myapp/index.html',context=context)
 
+def index(request):
+
+    li = ["Alllen", "Sreerag", "alwin", "justin", "allu"]
+
+    context = {'names': li}
+
+    return render(request, 'myapp/index.html', context=context)
 
 
 def new_one(request):
     return render(request, 'listing/new_one.html')
 
+
 def products(request):
-    p = Product.objects.get(name='I Phone')
-    
-    
-    return HttpResponse(p)
+    p = Product.objects.filter(price__gt = 100000)
+    context = {'products': p}
+
+    return render(request, 'myapp/products.html', context=context)
+
 
 def my_place(request):
-    
+
     return render(request, 'myplace/assignment.html')
