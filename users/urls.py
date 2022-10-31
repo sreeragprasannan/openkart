4f3c056bd4a5d22f76e80c882e5631cfb2116fef
sreerag <sreeraaag@gmail.com>
2022-10-31 19:50:31 +0530

    22th commit

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,8 @@ app_name = 'users'
 
 urlpatterns =[
     path('register/',register, name='register'),
-    path('login/',authentication_views.LoginView.as_view(template_name='users/login.html'),name='login')
+    path('login/',authentication_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/',authentication_views.LogoutView.as_view(template_name='users/logout.html'),name='logout')
     # path('products/',products,name='products'),
     # path('products/<int:id>/',products_detail,name='products_details'),
     # path('products/add',add_products,name='add_products'),
