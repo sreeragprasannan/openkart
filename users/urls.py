f3b355739ff771f335fcea228b55e036d14cdbe3
sreerag <sreeraaag@gmail.com>
2022-11-01 19:44:00 +0530

    23rd commit

--- users/urls.py
+++ users/urls.py
@@ -8,10 +8,5 @@ urlpatterns =[
     path('register/',register, name='register'),
     path('login/',authentication_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',authentication_views.LogoutView.as_view(template_name='users/logout.html'),name='logout')
-    # path('products/',products,name='products'),
-    # path('products/<int:id>/',products_detail,name='products_details'),
-    # path('products/add',add_products,name='add_products'),
-    # path('products/update/<int:id>',update_products,name='update_products'),
-    # path('products/delete/<int:id>',delete_products,name='delete_products'),
     
 ]
