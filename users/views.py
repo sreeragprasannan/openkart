d50be9d0b3f157514269d598717119034e96abf3
sreerag <sreeraaag@gmail.com>
2022-11-03 19:43:43 +0530

    25th commit

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from users.forms import NewUserForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 # Create your views here.
 
 #login forms
@@ -21,15 +22,24 @@ def register(request):
 
 def profile(request):
     
-    # context=('user')
+    pro = Profile.objects.get(user=request.user)
+    context={'profile':pro}
     
-    return render(request, 'users/profile.html')
+    return render(request,'users/profile.html',context=context)
 
+@login_required
 def create_profile(request):
     
     if request.method=='POST':
-        pass
+        contact_number = request.POST.get('contact_number')
+        image = request.FILES['upload']
+        pro = Profile()
+        pro.contact_number = contact_number
+        pro.image = image
+        pro.user = request.user
+        pro.save()
+        return redirect('/users/profile')
     
-    return render(request,'user/createprofile')
+    return render(request,'users/createprofile.html')
     
     
